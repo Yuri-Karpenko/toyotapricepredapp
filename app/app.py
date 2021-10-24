45a278c51c22601362aec5ea2553baa805ee5e8a
Yuri Karpenko <karpenko.yuriy@gmail.com>
2021-10-23 19:44:47 -0700

    fixing routing for heroku

--- app/app.py
+++ app/app.py
@@ -12,34 +12,28 @@ app = Flask(__name__)
 
 def PricePredictor(to_predict_list):
     to_predict = np.array(to_predict_list).reshape(1, 7)
-    loaded_model = load('model.joblib')
+    loaded_model = load('app/model.joblib')
     result = loaded_model.predict(to_predict)
     return result[0]
 
 
-def plot_features(columns, importances):
-    df = (pd.DataFrame({"features": columns,
-                        "feature_importances": importances}).sort_values("feature_importances", ascending=False).reset_index(drop=True))
-    fig, ax = plt.subplots()
-    ax.barh(df["features"], df["feature_importances"])
-
 def BuildVisuals():
-    df = pd.read_csv('toyota-cars-temp.csv')
+    df = pd.read_csv('app/toyota-cars-temp.csv')
     df_cur = df.drop(columns=['price'])
     for label, content in df_cur.items():
         if pd.api.types.is_string_dtype(content):
             df_cur[label] = content.astype("category").cat.as_ordered()
             df_cur[label] = pd.Categorical(content).codes
     fig = pl.hist_frame(df.price)
-    fig.write_image("static/datadistribution.svg", width=500)
+    fig.write_image("app/static/datadistribution.svg", width=500)
     correlation = df_cur.corr()
     fig_cor = px.imshow(correlation)
-    fig_cor.write_image("static/correlation_hist.svg", width=500)
-    model = load('model.joblib')
+    fig_cor.write_image("app/static/correlation_hist.svg", width=500)
+    model = load('app/model.joblib')
     importances = model.feature_importances_
     features_df = (pd.DataFrame({"features": df_cur.columns, "feature_importances": importances}).sort_values("feature_importances", ascending=False).reset_index(drop=True))
     fig_features = px.bar(features_df, x=features_df.features, y=features_df.feature_importances)
-    fig_features.write_image("static/feature_importances.svg", width=500)
+    fig_features.write_image("app/static/feature_importances.svg", width=500)
 
 @app.route('/result', methods=['POST'])
 def result():
